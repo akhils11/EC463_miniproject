6f889301035b9585f9197df8cf8875709f0dfdf4
akhils11 <asehgal1@bu.edu>
2022-09-12 16:46:15 -0400

    botometer score
    pull botometer score for specific user

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # EC463_miniproject
-# this is a test to see if it pushes
--- config.py
+++ config.py
@@ -4,10 +4,10 @@ import botometer
 
 rapidapi_key = "6c37f64ed3msh1133a7a0288b8efp14749bjsn53713384b4ff"
 twitter_app_auth = { 
-"consumer_key": "5gmn7aKPH2Auig4IzlfbJFiAL"
-"consumer_secret" : "42eLxX1cJHMiphKJ85UH8RtSm4TWpQeVBmPuDnGztQozeprwqV"
-"access_token" : "3487626854-WsHEpiyqitzDw3urb2a7jZ8eBCBXq6k538EaOHu"
-"access_token_secret" : "9wdcFRMtJoNW60lQDCkjJTEZmggoeY4eQv1fNHfwxG1E6"
+"consumer_key": "5gmn7aKPH2Auig4IzlfbJFiAL" , 
+"consumer_secret" : "42eLxX1cJHMiphKJ85UH8RtSm4TWpQeVBmPuDnGztQozeprwqV", 
+"access_token" : "3487626854-WsHEpiyqitzDw3urb2a7jZ8eBCBXq6k538EaOHu",
+"access_token_secret" : "9wdcFRMtJoNW60lQDCkjJTEZmggoeY4eQv1fNHfwxG1E6",
 }
 
 
