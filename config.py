15a81935dcf008592d175d1a1cdda1d3ae4add24
akhils11 <asehgal1@bu.edu>
2022-09-12 16:25:39 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 import tweepy
-
-consumer_key = "5gmn7aKPH2Auig4IzlfbJFiAL"
-consumer_secret = "42eLxX1cJHMiphKJ85UH8RtSm4TWpQeVBmPuDnGztQozeprwqV"
-access_token =  "3487626854-WsHEpiyqitzDw3urb2a7jZ8eBCBXq6k538EaOHu"
-access_token_secret = "9wdcFRMtJoNW60lQDCkjJTEZmggoeY4eQv1fNHfwxG1E6"
-
-auth = tweepy.OAuth1UserHandler(
-   consumer_key, consumer_secret, access_token, access_token_secret
-)
-
-api = tweepy.API(auth)
-
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print(tweet.text)
+import botometer
 
 
+rapidapi_key = "6c37f64ed3msh1133a7a0288b8efp14749bjsn53713384b4ff"
+twitter_app_auth = { 
+"consumer_key": "5gmn7aKPH2Auig4IzlfbJFiAL"
+"consumer_secret" : "42eLxX1cJHMiphKJ85UH8RtSm4TWpQeVBmPuDnGztQozeprwqV"
+"access_token" : "3487626854-WsHEpiyqitzDw3urb2a7jZ8eBCBXq6k538EaOHu"
+"access_token_secret" : "9wdcFRMtJoNW60lQDCkjJTEZmggoeY4eQv1fNHfwxG1E6"
+}
+
+
+bom = botometer.Botometer(wait_on_ratelimit=True,
+                          rapidapi_key=rapidapi_key, **twitter_app_auth)
+
+result = bom.check_account('@akhil_sehgal1')
+
+print(result)
